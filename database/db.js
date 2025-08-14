36b23e362bb4172cacb548c24c1ef8e999284808
benher908 <benkisher908@gmail.com>
2025-08-14 14:43:49 -0600

    cambios

--- database/db.js
+++ database/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.PORT || 5432, 
+  port: process.env.DB_PORT || 5432, 
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
